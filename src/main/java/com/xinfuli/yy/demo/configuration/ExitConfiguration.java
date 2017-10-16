2ad4a974f4a2e266b1c0e892996965ce0c572a13
Ilidan_Y <smile_yangy@163.com>
2017-10-16 18:10:23 +0800

    应用结束时返回特定的退出码

---
+++ src/main/java/com/xinfuli/yy/demo/configuration/ExitConfiguration.java
@@ -0,0 +1,17 @@
+package com.xinfuli.yy.demo.configuration;
+
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExitConfiguration implements ExitCodeGenerator {
+
+	/**
+	 * 应用结束时返回特定的退出码
+	 */
+	public int getExitCode() {
+		System.out.println("exitCode:"+0);
+		return 0;
+	}
+
+}
