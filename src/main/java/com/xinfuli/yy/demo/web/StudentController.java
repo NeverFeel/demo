68fbefba182bae09e4935817514d0c2016ed14a4
Ilidan_Y <smile_yangy@163.com>
2017-10-17 11:14:31 +0800

    1.properties文件；2.随机数配置

--- src/main/java/com/xinfuli/yy/demo/web/StudentController.java
+++ src/main/java/com/xinfuli/yy/demo/web/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xinfuli.yy.demo.configuration.NumberConfiguration;
 import com.xinfuli.yy.demo.domain.Student;
 import com.xinfuli.yy.demo.service.StudentService;
 
@@ -14,9 +15,16 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 	
+	@Autowired
+	private NumberConfiguration numberConfiguration;
+	
 	@RequestMapping(value="/getStudentById",method=RequestMethod.GET)
 	public Student getStudentById(){
 		return studentService.findStudentById("");
 	}
 	
+	@RequestMapping(value="/getRandomNumber",method=RequestMethod.GET)
+	public NumberConfiguration getRandomNumber(){
+		return numberConfiguration;
+	}
 }
