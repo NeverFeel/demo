58dac6de7cdcbea3a653f4f7e1e567e54e99f820
Ilidan_Y <smile_yangy@163.com>
2017-11-14 14:46:28 +0800

    换了baseMapper包名

--- src/main/java/com/xinfuli/yy/demo/baseMapper/BaseMapper.java
+++ src/main/java/com/xinfuli/yy/demo/baseMapper/BaseMapper.java
@@ -1,4 +1,4 @@
-package com.xinfuli.yy.demo.BaseMapper;
+package com.xinfuli.yy.demo.baseMapper;
 
 /**
  * 
--- src/main/java/com/xinfuli/yy/demo/mapper/StudentMapper.java
+++ src/main/java/com/xinfuli/yy/demo/mapper/StudentMapper.java
@@ -1,6 +1,6 @@
 package com.xinfuli.yy.demo.mapper;
 
-import com.xinfuli.yy.demo.BaseMapper.BaseMapper;
+import com.xinfuli.yy.demo.baseMapper.BaseMapper;
 import com.xinfuli.yy.demo.domain.Student;
 
 public interface StudentMapper extends BaseMapper<Student>{
